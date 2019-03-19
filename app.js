cd1db74f64436b5ea9edaf7b5aae653ce01f1450
damouryouknow <damourba@gmail.com>
2019-03-19 17:23:34 -0400

    Added Ace editor

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-(function($, hljs){
+(function($, hljs, ace){
+    var editors = [];
+
     $(document).ready(function() {
         $('.lesson-link').click(function() {
             let lessonPath = $(this).attr('path') + "lesson.md";
             loadFile(lessonPath, function(content) {
                 $('#lesson').html(marked(content));           
                 highlight();
+
+                // Initialize code editors.
+                for (let elem of document.getElementsByClassName('editor')) {
+                    let editor = ace.edit(elem);
+                    editor.setTheme("ace/theme/github");
+                    editor.session.setMode("ace/mode/javascript");
+                    editors.push(editor);
+                }
             });
         });
     }); 
@@ -23,4 +33,4 @@
             success: function(content) { callback(content); } 
         });
     }
-})(jQuery, hljs);
+})(jQuery, hljs, ace);
--- lesson2-test2/lesson.md
+++ lesson2-test2/lesson.md
@@ -1,3 +1,7 @@
 # Lesson 2 - Another Test!
 
 Let's validate that we can navigate through lessons!
+
+Here's the Ace editor component we are going to try and use:
+
+<div class="editor">let x = 7;</div>
