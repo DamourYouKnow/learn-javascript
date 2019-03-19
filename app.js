084cdc566802827f5f8950457b31ec15f5ec42b9
damouryouknow <damourba@gmail.com>
2019-03-19 19:42:28 -0400

    Working code executer

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
+var output = {
+    'index': 0,
+    'log': function(x) {
+        let area = $('.output-area').eq(0);
+        if (area) {
+            area.val(`${area.val()}\n${x.valueOf()}`);
+        }
+    }
+};
+
 (function($, hljs, ace){
     var editors = [];
 
@@ -24,14 +39,39 @@
         editor.session.setMode("ace/mode/javascript");
         editors.push(editor);
 
-        let runBtn = $('<button>').text("Run!");
+        let runBtn = $('<button>')
+                .addClass('run-btn')
+                .text("Run!")
+                .click(runCode);
+
         let outputArea = $('<textarea>')
-                .addClass('output-area')/*.prop('readonly')*/;
+                .addClass('output-area')
+                .attr('readonly', true)
+                .val("Your output will show up here...");
 
         elem.append(editorPane);
         elem.append(runBtn);
         elem.append(outputArea);
+    }
 
+    function runCode() {
+        let editor = $(this).parent('.editor');
+
+        $('.editor').each(function(i) {
+            if ($(this).is(editor)) {
+                output.index = i;
+
+                $('.output-area').eq(i).val("Your output will show up here...");
+                editor.remove('.script-area');
+
+                let content = editors[i].getSession().getValue();
+                content = content.replaceAll("console.log", "output.log")
+
+                let scriptArea = $('<script>').addClass('script-area');
+                scriptArea.html(content);
+                editor.append(scriptArea);
+            }
+        });
     }
 
     function highlight() {
